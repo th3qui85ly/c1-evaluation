c6cb031e810c1ac176b168548b0c19219a3b18a2
= <=>
2022-04-18 21:11:05 +0530

    checkPermission middleware updated

--- app.js
+++ app.js
@@ -1,39 +1,69 @@
 const express = require("express");
 
 const app = express();
+app.use(express.json());
 const PORT = 1912;
 
+// Path Logger for logging paths of every routing
 let logger = (req, res, next) => {
-    console.log("This is logger for all req");
-    let reqPath = req.url;
-    let log = `${reqPath}`;
-    console.log(log);
+    console.log("App is logging at " + req.path);
     next();
 };
 
 app.use(logger);
 
+
+// "checkPermission" middleware to add "status : true" for "/libraries" and "/authors" routes.
+const checkPermission = (req, res, next) => {
+    if (req.params.name == "librarian" || req.params.name == "author" || req.url == "/libraries" || req.url == "/authors") {
+        req.status = true;
+    } else
+        req.status = false;
+    next();
+};
+
+// Routes -
+/*
+    "/"          - Home page
+    "/books"     - Books route
+    "/libraries" - Library route & "/libraries/:name" - Passing a name to base route
+    "/authors"   - Author route &  "/authors/:name" - Passing a name to base route
+*/
 app.get("/", (req, res, next) => {
     res.send("Welcome to Home Page! :))");
 });
 
 app.get("/books", (req, res, next) => {
     res.status(200).json({
-        route: "/books"
+        route: req.url,
     });
 });
 
-app.get("/libraries", (req, res, next) => {
+app.get("/libraries", checkPermission, (req, res, next) => {
     res.status(200).json({
-        route: "/libraries",
-        permission: true
+        route: req.url,
+        permission: req.status,
     });
 });
 
-app.get("/authors", (req, res, next) => {
+app.get("/libraries/:name", checkPermission, (req, res, next) => {
     res.status(200).json({
-        route: "/authors",
-        permission: true
+        route: req.url,
+        permission: req.status,
+    });
+});
+
+app.get("/authors", checkPermission, (req, res, next) => {
+    res.status(200).json({
+        route: req.url,
+        permission: req.status,
+    });
+});
+
+app.get("/authors/:name", checkPermission, (req, res, next) => {
+    res.status(200).json({
+        route: req.url,
+        permission: req.status,
     });
 });
 
